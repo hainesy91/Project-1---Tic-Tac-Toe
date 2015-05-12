d2b14fd0756ed05a7977814364b2602b9d0933a4
Christopher Haines <hainesy91@gmail.com>
2015-05-12 13:56:46 +0100

    added buttons

--- jquery.js
+++ jquery.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+$(document).ready(function(){
+
+
+$(".firstRow").click(function(){
+  alert("You have clicked the first row");
+});
+
+$(".secondRow").click(function(){
+  alert("You have clicked the second row");
+});
+
+$(".thirdRow").click(function(){
+  alert("You have clicked the third row");
+});
+
+})
