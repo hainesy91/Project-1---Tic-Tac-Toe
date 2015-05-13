eb0740a4fa7c0bc05a7803aa8712ea567a05f34f
Christopher Haines <hainesy91@gmail.com>
2015-05-13 14:24:44 +0100

    got the input to be recognised as the number values in the console instead of strings

--- jquery.js
+++ jquery.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 
 var lastMove ="O"
-var winningCombo = ["one, two, three", "four five six", "seven eight nine", "one four seven", "two five nine", "three six nine", "three five eight", "one five nine"];
-var arrayLength = winningCombo.length;
+var winningCombo = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 9], [3, 6, 9], [3, 5, 8], [1, 5, 9]];
+var winningArray = []
+var xMoves = []
+var oMoves = []
 
 
 $(".gridBox").on("click", function(){
   if(lastMove === "X") {
     $(this).text("O");
     lastMove = "O"
+    var id = $(this).attr('id')
+    oMoves.push(Number(id))
   }
    else{
       $(this).text("X")
       lastMove = "X"
+      var id = $(this).attr('id')
+      xMoves.push(Number(id))
     }  
 })
-
-for (var i = 0; i < arrayLength; i++) {
-  $(click(winningCombo[i])
-}
-
-
 
 
 
