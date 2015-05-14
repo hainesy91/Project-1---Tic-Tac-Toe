9416943a3aade08af88fb31ea7c2c6dd58719385
Christopher Haines <hainesy91@gmail.com>
2015-05-14 09:40:36 +0100

    added an empty div that declares who the winner is when they have won

--- jquery.js
+++ jquery.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 
 var lastMove ="O"
-var winningCombo = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 9], [3, 6, 9], [3, 5, 8], [1, 5, 9]];
-var winningArray = []
+var winningCombo = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 5, 9]];
 var xMoves = []
 var oMoves = []
 
@@ -13,15 +12,36 @@ $(".gridBox").on("click", function(){
     lastMove = "O"
     var id = $(this).attr('id')
     oMoves.push(Number(id))
+    console.log(oMoves)
+    checkForWinner(oMoves);
   }
    else{
       $(this).text("X")
       lastMove = "X"
       var id = $(this).attr('id')
       xMoves.push(Number(id))
+      console.log(xMoves)
+      checkForWinner(xMoves);
     }  
 })
 
+function checkForWinner(playerArray){
+  var orderedArray = playerArray.sort()
+  console.log(orderedArray[0])
+  for(i = 0; i < winningCombo.length -1; i++){
+    for(j = 0; j < orderedArray.length -1; j++){
+      if(orderedArray[j] === winningCombo[i][0]){
+        if(winningCombo[i][1] === orderedArray[j+1] && winningCombo[i][2] === orderedArray[j+2]){
+          $('div.Winner').text(lastMove + ' ' + "is the winner!");
+            
+          }
+        }
+        // console.log(winningCombo[i])
+        
+    }
+  }    //console.log(winningCombo[i])
+    //}
 
+  }
 
 })
